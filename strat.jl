166be2f8bae75d8e5fe93e945e5934c472c1d61e
Jason Xi Yang <xyang40@alumni.jh.edu>
2019-04-11 16:24:23 -0400

    Update strat.jl

--- strat.jl
+++ strat.jl
@@ -2,17 +2,22 @@ using CSV
 using DataFrames
 
 etfs = Dict("EWJ"=>"Japan","EWZ"=>"Brazil","FXI"=>"China","EWY"=>"South Korea",
-"INDA"=>"India","EWT"=>"Taiwan","EWH"=>"Hong Kong","EWC"=>"Canada","EWG"=>"Germany",
+"EWT"=>"Taiwan","EWH"=>"Hong Kong","EWC"=>"Canada","EWG"=>"Germany",
 "EWU"=>"United Kingdom","EWA"=>"Australia","EWW"=>"Mexico","EWL"=>"Switzerland",
 "EWP"=>"Spain","EWQ"=>"France","EIDO"=>"Indonesia","ERUS"=>"Russia","EWS"=>"Singapore",
-"EWM"=>"Malaysia","EZA"=>"South Africa","KSA"=>"Saudi Arabia","THD"=>"Thailand",
+"EWM"=>"Malaysia","EZA"=>"South Africa","THD"=>"Thailand",
 "ECH"=>"Chile","EWI"=>"Italy","TUR"=>"Turkey","EPOL"=>"Poland","EPHE"=>"Philippines",
 "EWD"=>"Sweden","EWN"=>"Netherlands","EPU"=>"Peru","ENZL"=>"New Zealand",
-"EIS"=>"Israel","EWO"=>"Austria","EIRL"=>"Ireland","EWK"=>"Belgium","QAT"=>"Qatar",
-"UAE"=>"UAE","EDEN"=>"Denmark")
+"EIS"=>"Israel","EWO"=>"Austria","EIRL"=>"Ireland","EWK"=>"Belgium")
 
-#CSV.read()
+tickers = [k for k in keys(etfs)]
 
-df = CSV.File("files\\ech.csv") |> DataFrame
-
-#Date	Open	High	Low	Close	Adj Close	Volume
+df = CSV.File("files\\"*tickers[1]*".csv") |> DataFrame
+df = df[:, [Symbol("Date"), Symbol("Adj Close")]]
+for ticker in tickers[2:end,]
+    #println(ticker)
+    curr = CSV.File("files\\"*ticker*".csv") |> DataFrame
+    #println(curr)
+    join(df, curr[:, [Symbol("Date"), Symbol("Adj Close")]], on = :Date, makeunique=true)
+    #df[:ticker] = curr[:,[Symbol("Adj Close")]]
+end
