5ec5551dab6ac7ceb59fbfff74354fde0353c28a
Jason Xi Yang <xyang40@alumni.jh.edu>
2019-04-22 20:50:08 -0400

    updates scripts and datafile

--- strat.jl
+++ strat.jl
@@ -12,18 +12,5 @@ etfs = Dict("EWJ"=>"Japan","EWZ"=>"Brazil","FXI"=>"China","EWY"=>"South Korea",
 
 tickers = [k for k in keys(etfs)]
 
-frame = DataFrame()
-df = CSV.File("C:\\Users\\Xi\\Desktop\\Projects\\ETFpairstrading\\files\\"*tickers[1]*".csv") |> DataFrame
-#df = df[:, [Symbol("Date"), Symbol("Adj Close")]]
-df = df[:, [Symbol("Adj Close")]]
-push!(frame, df')
-#matrix = Matrix(2013, 0)
-for ticker in tickers[2:end,]
-    println(ticker)
-    curr = CSV.File("C:\\Users\\Xi\\Desktop\\Projects\\ETFpairstrading\\files\\"*ticker*".csv") |> DataFrame
-    #println(curr)
-    #join(df, curr[:, [Symbol("Date"), Symbol("Adj Close")]], on = :Date, makeunique=true)
-    #df[:ticker] = curr[:,[Symbol("Adj Close")]]
-    adjclose = curr[:, [Symbol("Adj Close")]]
-    df = [df adjclose]
-end
+df = CSV.File("C:\\Users\\Xi\\Desktop\\Projects\\ETFpairstrading\\files\\ALL.csv") |> DataFrame
+print(size(df))
