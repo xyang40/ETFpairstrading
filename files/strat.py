eb3e58690131a0224024df43cc7c684a3aaa337d
Jason Xi Yang <xyang40@alumni.jh.edu>
2019-05-11 16:22:33 -0400

    add in python supports

---
+++ files/strat.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import covariance
+
+
+etfs = {"EWJ":"Japan","EWZ":"Brazil","FXI":"China","EWY":"South Korea",
+"EWT":"Taiwan","EWH":"Hong Kong","EWC":"Canada","EWG":"Germany",
+"EWU":"United Kingdom","EWA":"Australia","EWW":"Mexico","EWL":"Switzerland",
+"EWP":"Spain","EWQ":"France","EIDO":"Indonesia","ERUS":"Russia","EWS":"Singapore",
+"EWM":"Malaysia","EZA":"South Africa","THD":"Thailand",
+"ECH":"Chile","EWI":"Italy","TUR":"Turkey","EPOL":"Poland","EPHE":"Philippines",
+"EWD":"Sweden","EWN":"Netherlands","EPU":"Peru","ENZL":"New Zealand",
+"EIS":"Israel","EWO":"Austria","EIRL":"Ireland","EWK":"Belgium"}
+
+df = pd.read_csv("C:\\Users\\Xi\\Desktop\\Projects\\ETFpairstrading\\files\\ALL.csv")
+del df['Date']
+edge_model = covariance.GraphicalLassoCV(cv=5)
+
+corr = df.corr()
+print(corr.shape)
